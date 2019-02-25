06662ec4ff047dedc8f5706b5bc0a61fd1a6f0cb
Sam Bostock <sam.bostock@shopify.com>
2019-02-24 20:40:33 -0500

    Add module access modifier specs

--- spec/rubocop/cop/style/access_modifier_declarations_spec.rb
+++ spec/rubocop/cop/style/access_modifier_declarations_spec.rb
@@ -16,6 +16,11 @@ RSpec.describe RuboCop::Cop::Style::AccessModifierDeclarations, :config do
         access_modifier: access_modifier,
         construct: :class
       )
+      it_behaves_like(
+        'enforces group access modifier usage',
+        access_modifier: access_modifier,
+        construct: :module
+      )
     end
   end
 
@@ -31,6 +36,11 @@ RSpec.describe RuboCop::Cop::Style::AccessModifierDeclarations, :config do
         'enforces inline access modifier usage',
         access_modifier: access_modifier,
         construct: :class
+      )
+      it_behaves_like(
+        'enforces inline access modifier usage',
+        access_modifier: access_modifier,
+        construct: :module
       )
     end
   end
